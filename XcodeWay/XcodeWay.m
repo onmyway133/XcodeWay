05e60112653742f366e83793ae5e51174047345c
Khoa Pham <onmyway133@gmail.com>
2014-08-23 18:02:40 +0700

    Setup Menu Items

--- XcodeWay/XcodeWay.m
+++ XcodeWay/XcodeWay.m
@@ -35,30 +35,130 @@ static XcodeWay *sharedPlugin;
         // reference to plugin's bundle, for resource acccess
         self.bundle = plugin;
 
-        // Create menu items, initialize UI, etc.
-
-        // Sample Menu Item:
-        NSMenuItem *menuItem = [[NSApp mainMenu] itemWithTitle:@"File"];
-        if (menuItem) {
-            [[menuItem submenu] addItem:[NSMenuItem separatorItem]];
-            NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"Do Action" action:@selector(doMenuAction) keyEquivalent:@""];
-            [actionMenuItem setTarget:self];
-            [[menuItem submenu] addItem:actionMenuItem];
-        }
+        [self registerNotifications];
     }
     return self;
 }
 
-// Sample Action, for menu item:
-- (void)doMenuAction
+- (void)dealloc
+{
+    [self unregisterNotifications];
+}
+
+#pragma mark - Notification
+- (void)registerNotifications
+{
+    NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
+
+    [notificationCenter addObserver:self
+                           selector:@selector(applicationDidFinishLaunching:)
+                               name:NSApplicationDidFinishLaunchingNotification
+                             object:NSApp];
+}
+
+- (void)unregisterNotifications
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)notification
+{
+    // Application did finish launching is only send once. We do not need it anymore.
+    NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
+    [notificationCenter removeObserver:self
+                                  name:NSApplicationDidFinishLaunchingNotification
+                                object:NSApp];
+
+    [self setupMenu];
+}
+
+#pragma mark - Setup Menu
+- (void)setupMenu
+{
+    // Navigate
+    NSMenuItem *navigateMenuItem = [[NSApp mainMenu] itemWithTitle:@"Window"];
+    if (navigateMenuItem) {
+        // Separator
+        [[navigateMenuItem submenu] addItem:[NSMenuItem separatorItem]];
+
+        // XcodeWay
+        NSMenuItem *xcodeWayMenuItem = [[NSMenuItem alloc] initWithTitle:@"XcodeWay"
+                                                                  action:nil
+                                                           keyEquivalent:@""];
+        xcodeWayMenuItem.enabled = YES;
+        [xcodeWayMenuItem setTarget:self];
+        [[navigateMenuItem submenu] addItem:xcodeWayMenuItem];
+        xcodeWayMenuItem.submenu = [[NSMenu alloc] initWithTitle:xcodeWayMenuItem.title];
+
+        // Sub Menu Item
+        [self addMenuItemNamed:@"Go To Project Folder" action:@selector(gotoProjectFolder:) intoParentMenuItem:xcodeWayMenuItem];
+        [self addMenuItemNamed:@"Go To Simulator Folder" action:@selector(goToSimulatorFolder:) intoParentMenuItem:xcodeWayMenuItem];
+        [self addMenuItemNamed:@"Go To Derived Data Folder" action:@selector(goToDerivedDataFolder:) intoParentMenuItem:xcodeWayMenuItem];
+        [self addMenuItemNamed:@"Go To Plug-Ins Folder" action:@selector(goToPlugInsFolder:) intoParentMenuItem:xcodeWayMenuItem];
+        [self addMenuItemNamed:@"Go To Template Folder" action:@selector(goToTemplateFolder:) intoParentMenuItem:xcodeWayMenuItem];
+
+        [[xcodeWayMenuItem submenu] addItem:[NSMenuItem separatorItem]];
+
+        [self addMenuItemNamed:@"Go To Terminal" action:@selector(goToTerminal:) intoParentMenuItem:xcodeWayMenuItem];
+
+        [[xcodeWayMenuItem submenu] addItem:[NSMenuItem separatorItem]];
+
+        [self addMenuItemNamed:@"Go To Github" action:@selector(goToGithub:) intoParentMenuItem:xcodeWayMenuItem];
+        [self addMenuItemNamed:@"Go To Bitbucket" action:@selector(goToBitbucket:) intoParentMenuItem:xcodeWayMenuItem];
+    }
+}
+
+- (void)addMenuItemNamed:(NSString *)name action:(SEL)action intoParentMenuItem:(NSMenuItem *)parentMenuItem
+{
+    NSMenuItem *menuItem = [[NSMenuItem alloc] initWithTitle:name
+                                                      action:action
+                                               keyEquivalent:@""];
+
+    [menuItem setTarget:self];
+    [[parentMenuItem submenu] addItem:menuItem];
+}
+
+#pragma mark - Menu Action
+- (void)gotoProjectFolder:(id)sender
 {
     NSAlert *alert = [NSAlert alertWithMessageText:@"Hello, World" defaultButton:nil alternateButton:nil otherButton:nil informativeTextWithFormat:@""];
     [alert runModal];
 }
 
-- (void)dealloc
+- (void)goToSimulatorFolder:(id)sender
 {
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
+
 }
+
+- (void)goToDerivedDataFolder:(id)sender
+{
+
+}
+
+- (void)goToPlugInsFolder:(id)sender
+{
+
+}
+
+- (void)goToTemplateFolder:(id)sender
+{
+
+}
+
+- (void)goToTerminal:(id)sender
+{
+
+}
+
+- (void)goToGithub:(id)sender
+{
+
+}
+
+- (void)goToBitbucket:(id)sender
+{
+
+}
+
 
 @end
