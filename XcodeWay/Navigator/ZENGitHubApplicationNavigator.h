89255eeb4417586c3fabe9fbb1e3a92e1dff322c
Christoffer Winterkvist <chris@zenangst.com>
2015-02-04 08:30:52 +0100

    Add ZENGitHubApplicationNavigator
    Navigate to the current Xcode directory in GitHub application

---
+++ XcodeWay/Navigator/ZENGitHubApplicationNavigator.h
@@ -0,0 +1,13 @@
+//
+//  ZENGitHubApplicationNavigator.h
+//  XcodeWay
+//
+//  Created by Christoffer Winterkvist on 04/02/15.
+//  Copyright (c) 2015 Fantageek. All rights reserved.
+//
+
+#import "FTGNavigator.h"
+
+@interface ZENGitHubApplicationNavigator : FTGNavigator
+
+@end
