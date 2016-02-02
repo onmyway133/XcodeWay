beb71d9e97fd3517602df968cb2ed7a436bc9b82
Christoffer Winterkvist <christoffer@hyper.no>
2016-02-02 08:49:12 +0100

    Refactor opening iTerm

--- XcodeWay/Navigator/FTGiTermNavigator.m
+++ XcodeWay/Navigator/FTGiTermNavigator.m
@@ -12,15 +12,9 @@
 
 - (void)navigate
 {
-    NSString *projectPath = [[FTGEnvironmentManager sharedManager] projectPath];
-    NSString *projectFolderPath = [projectPath stringByDeletingLastPathComponent];
-
-    NSString *iTermPath = @"/Applications/iTerm.app/Contents/MacOS/iTerm";
-    if ([[NSFileManager defaultManager] fileExistsAtPath:iTermPath]) {
-        [NSTask ftg_runTaskWithLaunchPath:iTermPath
-                                arguments:@[ projectFolderPath ]];
-    } else {
-        [NSAlert ftg_showMessage:@"iTerm.app is expected to be in Applications folder"];
+    BOOL launchediTerm = [[NSWorkspace sharedWorkspace] launchApplication:@"iTerm"];
+    if (launchediTerm == NO) {
+        [NSAlert ftg_showMessage:@"Could not launch iTerm"];
     }
 }
 
