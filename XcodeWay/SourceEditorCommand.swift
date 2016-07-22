243f28ffdd87e1511c8d901a1023dcf509fe10af
Khoa Pham <onmyway133@gmail.com>
2016-07-22 11:56:41 +0200

    Reindent

--- XcodeWay/SourceEditorCommand.swift
+++ XcodeWay/SourceEditorCommand.swift
@@ -10,11 +10,11 @@ import Foundation
 import XcodeKit
 
 class SourceEditorCommand: NSObject, XCSourceEditorCommand {
-    
-    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: (NSError?) -> Void ) -> Void {
-        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
-        
-        completionHandler(nil)
-    }
-    
+
+  func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: (NSError?) -> Void ) -> Void {
+    // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+
+    completionHandler(nil)
+  }
+
 }
--- XcodeWay/SourceEditorExtension.swift
+++ XcodeWay/SourceEditorExtension.swift
@@ -10,18 +10,16 @@ import Foundation
 import XcodeKit
 
 class SourceEditorExtension: NSObject, XCSourceEditorExtension {
-    
-    /*
-    func extensionDidFinishLaunching() {
-        // If your extension needs to do any work at launch, implement this optional method.
-    }
-    */
-    
-    /*
-    var commandDefinitions: [[XCSourceEditorCommandDefinitionKey: AnyObject]] {
-        // If your extension needs to return a collection of command definitions that differs from those in its Info.plist, implement this optional property getter.
-        return []
-    }
-    */
-    
+
+  /*
+   func extensionDidFinishLaunching() {
+   // If your extension needs to do any work at launch, implement this optional method.
+   }
+   */
+
+
+  var commandDefinitions: [[XCSourceEditorCommandDefinitionKey: AnyObject]] {
+    // If your extension needs to return a collection of command definitions that differs from those in its Info.plist, implement this optional property getter.
+    return []
+  }
 }
