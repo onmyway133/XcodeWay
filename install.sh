010c94c4b8d14c14cc1ad899dc20ede24816a08b
Khoa Pham <onmyway133@gmail.com>
2017-10-18 21:11:22 +0200

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -euo pipefail
+
+DOWNLOAD_URL=https://raw.githubusercontent.com/onmyway133/XcodeWay/master/XcodeWayExtensions/Script/XcodeWayScript.scpt
+SCRIPT_DIR="${HOME}/Library/Application Scripts/com.fantageek.XcodeWayApp.XcodeWayExtensions"
+
+mkdir -p "${SCRIPT_DIR}"
+curl $DOWNLOAD_URL -o "${SCRIPT_DIR}"
