f2cca1bc4d93dcf6ccbcca19b9c32dc44ff9af3f
Khoa Pham <onmyway133@gmail.com>
2019-04-10 23:01:53 +0200

    Check terminal

--- XcodeWayApp/ViewController.swift
+++ XcodeWayApp/ViewController.swift
@@ -12,12 +12,13 @@ class ViewController: NSViewController {
     let defaults = UserDefaults(suiteName: "T78DK947F2.group.com.onmyway133.XcodeWay")
 
     @IBOutlet weak var statusLabel: NSTextField!
+    @IBOutlet weak var terminalButton: NSButton!
+    @IBOutlet weak var itermButton: NSButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        defaults?.set("iTerm", forKey: "whichTerminal")
-        defaults?.synchronize()
+        checkInstallScript()
+        checkTerminal()
     }
     
     @IBAction func onButtonTouch(_ sender: NSButton) {
@@ -25,10 +26,38 @@ class ViewController: NSViewController {
     }
     
     @IBAction func onCheckTouch(_ sender: NSButton) {
+        checkInstallScript()
+    }
+    
+    func checkInstallScript() {
         let home = NSSearchPathForDirectoriesInDomains(.applicationScriptsDirectory, .userDomainMask, true).first!
         let path = home.appending(".XcodeWayExtensions/XcodeWayScript.scpt")
         let exists = FileManager.default.fileExists(atPath: path)
         statusLabel.stringValue = exists ? "Yes" : "No"
+    }
+    
+    func checkTerminal() {
+        let terminal = defaults?.string(forKey: "whichTerminal") ?? "Terminal"
+        
+        switch terminal {
+        case "iTerm":
+            itermButton.state = .on
+        default:
+            terminalButton.state = .on
+        }
+    }
+    
+    @IBAction func onRadioTouch(_ sender: NSButton) {
+        switch sender.tag {
+        case 0:
+            defaults?.set("Terminal", forKey: "whichTerminal")
+            defaults?.synchronize()
+        case 1:
+            defaults?.set("iTerm", forKey: "whichTerminal")
+            defaults?.synchronize()
+        default:
+            break
+        }
     }
 }
 
