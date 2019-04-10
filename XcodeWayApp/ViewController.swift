6466d436a05b1622e70b766fb72038fd4f217ad2
Khoa Pham <onmyway133@gmail.com>
2019-04-10 22:21:39 +0200

    Add visit project button

--- XcodeWayApp/ViewController.swift
+++ XcodeWayApp/ViewController.swift
@@ -9,16 +9,20 @@
 import Cocoa
 
 class ViewController: NSViewController {
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    // Do any additional setup after loading the view.
-  }
-
-  override var representedObject: Any? {
-    didSet {
-      // Update the view, if already loaded.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
     }
-  }
+    
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+    
+    @IBAction func onButtonTouch(_ sender: NSButton) {
+        NSWorkspace.shared.open(URL(string: "https://github.com/onmyway133/XcodeWay")!)
+    }
 }
 
